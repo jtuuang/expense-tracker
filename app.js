fb74d7cfa07abe543f40f86c9976cd925d761e69
jtuuang <jt.uuang@gmail.com>
2020-11-23 19:04:58 +0800

    Add delete function

--- app.js
+++ app.js
@@ -66,6 +66,14 @@ app.put('/records/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.delete('/records/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(3000, () => {
   console.log('The server is running on http://localhost:3000')
 })
