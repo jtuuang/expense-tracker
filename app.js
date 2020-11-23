30dd54c0f5ecbc6250469bde664728866036feb7
jtuuang <jt.uuang@gmail.com>
2020-11-23 18:19:11 +0800

    Add main.handlebars index.handlebars

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const Record = require('./models/record')
+
+require('./config/mongoose')
 
 const app = express()
 
@@ -11,3 +14,19 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  return Record.find()
+    .lean()
+    .sort({ _id: 'asc' })
+    .then(records => {
+      let totalAmount = 0
+      records.forEach(record => totalAmount += record.amount)
+      res.render('index', { records, totalAmount })
+    })
+    .catch(error => console.log(error))
+})
+
+app.listen(3000, () => {
+  console.log('The server is running on http://localhost:3000')
+})
