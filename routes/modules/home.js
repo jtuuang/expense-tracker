767db5003b92b37c1c82a24ec44b77a8cb04a8b5
jtuuang <jt.uuang@gmail.com>
2020-11-23 21:02:45 +0800

    Add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,16 +2,35 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 
-router.get('/', (req, res) => {
+function checkSort(sort, res) {
   return Record.find()
     .lean()
     .sort({ _id: 'asc' })
+    .then(records => records.filter(record => record.category === sort))
     .then(records => {
       let totalAmount = 0
       records.forEach(record => totalAmount += record.amount)
       res.render('index', { records, totalAmount })
     })
-    .catch(error => console.log(error))
+    .catch(error => console.log('error'))
+}
+
+router.get('/', (req, res) => {
+  const sort = req.query.sort
+
+  if (sort) {
+    checkSort(sort, res)
+  } else {
+    return Record.find()
+      .lean()
+      .sort({ _id: 'asc' })
+      .then(records => {
+        let totalAmount = 0
+        records.forEach(record => totalAmount += record.amount)
+        res.render('index', { records, totalAmount })
+      })
+      .catch(error => console.log(error))
+  }
 })
 
 module.exports = router
