ebcebd39ef8abd31d734c750e14e7c6be6a8108d
jtuuang <jt.uuang@gmail.com>
2020-11-23 19:21:23 +0800

    Modfiy record.js recordSeeder.js

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -6,31 +6,31 @@ db.once('open', () => {
   Record.create({
     name: '午餐',
     category: '餐飲食品',
-    date: '2019/04/23',
+    date: '2019-04-23',
     amount: 60
   })
   Record.create({
     name: '晚餐',
     category: '餐飲食品',
-    date: '2019/04/23',
+    date: '2019-04-23',
     amount: 60
   })
   Record.create({
     name: '捷運',
     category: '交通出行',
-    date: '2019/04/23',
+    date: '2019-04-23',
     amount: 120
   })
   Record.create({
     name: '電影：驚奇隊長',
     category: '休閒娛樂',
-    date: '2019/04/23',
+    date: '2019-04-23',
     amount: 220
   })
   Record.create({
     name: '租金',
     category: '家居物業',
-    date: '2019/04/01',
+    date: '2019-04-01',
     amount: 25000
   })
   console.log('done')
